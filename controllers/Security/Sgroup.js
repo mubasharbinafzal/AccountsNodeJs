222cd8b3d2ee9f9c59a91067754a9da6c47c3b69
Mubashar <mubasharbinafzal@gmail.com>
2022-03-07 18:55:07 +0500

    package.json added and complete sgroup API with validation

--- controllers/Security/Sgroup.js
+++ controllers/Security/Sgroup.js
@@ -6,20 +6,28 @@ const CustomError = require("../../utils/customError");
 
 class SgroupController {
   async create(req, res) {   
+    // validation 
    const validatedData = Validations.sgroup.validate(req.body); 
    if (validatedData.error) throw res.status(422).send(validatedData);
-  const result = await new Sgroup(validatedData).save(); 
-  res.status(200).send(response(STRINGS.TEXTS.SgroupCreated, result));
+   // check unique
+   const uniqueGroup = await Sgroup.findOne({grpTitle: validatedData.grpTitle});
+   if(uniqueGroup) throw res.status(422).send(STRINGS.ERRORS.ONLYUNIQUE);
+   // save In Db 
+   const result = await new Sgroup(validatedData).save(); 
+   if (!result) throw  res.status(422).send(result);
+   res.status(200).send(response(STRINGS.TEXTS.SgroupCreated, result));
   }
 
   async getAll(req, res) {   
-   const result = await Sgroup.find({}, { __v: 0 }); 
+   //Find all
+    const result = await Sgroup.find({}, { __v: 0 }); 
+
    res.status(200).send(response(STRINGS.TEXTS.GetAllSgroup, result));
    }
 
    async getOne(req, res) {
     const id = req.params.id;
-
+    // findOne 
     const result = await Sgroup.findOne({ _id: id }, { __v: 0 });
     if (!result) throw new CustomError(STRINGS.ERRORS.NOTFOUND);
 
@@ -27,17 +35,20 @@ class SgroupController {
   }
   async delete(req, res) {
     const id = req.params.id;
-
+    // delete
     const result = await Sgroup.findByIdAndDelete(id);
 
     res.status(200).send(response(STRINGS.TEXTS.SgroupDeleted , result));
   }
   async Update(req, res) {   
     const id = req.params.id;
+    // validation 
     const validatedData = Validations.sgroup.validate(req.body); 
-    if (validatedData.error){
-     res.status(422).send(validatedData);
-    } 
+    if (validatedData.error) throw res.status(422).send(validatedData); 
+    // check unique
+   const uniqueGroup = await Sgroup.findOne({grpTitle: validatedData.grpTitle});
+   if(uniqueGroup) throw res.status(422).send(STRINGS.ERRORS.ONLYUNIQUE);
+    // findAndUpdate
     const result = await Sgroup.findByIdAndUpdate(
         { _id: id },
         { $set: validatedData },
