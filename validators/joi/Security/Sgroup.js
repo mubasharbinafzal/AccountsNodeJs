90a8d693fee62be676cad7d6e3a17f804b4ec110
Mubashar <mubasharbinafzal@gmail.com>
2022-03-22 19:12:47 +0500

    sgroup validation

--- validators/joi/Security/Sgroup.js
+++ validators/joi/Security/Sgroup.js
@@ -5,13 +5,13 @@ const CustomError = require("../../../utils/customError");
 const schemas = {
     validate: (data) => {
     const Validation = Joi.object().keys({ 
-        grpTitle: Joi.string().min(3).max(25).required(),  
+        grpTitle: Joi.string().min(3).max(25).required().label('Group Title')  
        
     }); 
     const { error, value } = Validation.validate(data); 
     if (error) { 
       const { details } = error;
-      const message = details.map((i) => i.message).join(",");  
+      const message = details.map((i) => i.message).join(",").replace(/\"/gi, '');    
         return {
             error: 422,
             message:message,
